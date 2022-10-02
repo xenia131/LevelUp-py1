c529f0a723ceab1e8be12903fea9c5e76c1405f0
Ксения Барашкова <ikt.2109@ya.ru>
2022-10-02 20:23:21 +0300

    upd headlines

--- hw-1.1.py
+++ hw-1.1.py
@@ -1,9 +1,7 @@
-month = int(input())
-list1 = [1,3,5,7,8,10,12]
-list2 = [4,6,9,11]
-if month in list1:
-    print("31")
-elif month in list2:
-    print("30")
+n = float(input())
+if n>0:
+    print("positive")
+elif n==0:
+    print("zero")
 else:
-    print("FEBRUARY!")
+    print("negative")
