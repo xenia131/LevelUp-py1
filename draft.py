d002967ad8d401a2774418512c86a500081cfd3f
Ксения Барашкова <ikt.2109@ya.ru>
2022-10-04 20:16:50 +0300

    upd a-format

--- draft.py
+++ draft.py
@@ -1,9 +1,9 @@
-a = [input() for i in range(int(input()))]
+a = [int(input()) for i in range(int(input()))]
 q = []
 even = []
 #a = [int(x) for x in input().split()]
 for i in range(len(a)):
-    t = int(a[i])
+    t = a[i]
     q.append(t*t)
     if t%2 ==0:
         even.append(t)
