c1b5ff92eb539cd232f7609ee029d80b590dd8bd
Ксения Барашкова <ikt.2109@ya.ru>
2022-10-14 19:25:53 +0300

    input was commented

--- 10-14-2.py
+++ 10-14-2.py
@@ -1,10 +1,10 @@
 def countRange(list, min, max):
     output = []
-    list = [input("enter numbers: ") for i in range(int(input("enter n: ")))]
-    min, max = float(input("enter min: ")), float(input("enter max: "))
+    #list = [input("enter numbers: ") for i in range(int(input("enter n: ")))]
+    #min, max = float(input("enter min: ")), float(input("enter max: "))
     for i in range(len(list)):
         if round(float(list[i])) in range((int(min)),int(max)):
             output.append(list[i])  
     return(output)
 
-print(countRange(list, min, max))
+print(countRange([1,2,3,4,5,6,7], 2, 6))
