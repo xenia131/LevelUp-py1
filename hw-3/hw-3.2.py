788581aaae9380d4a2bb536509648adfc7340c38
Ксения Барашкова <ikt.2109@ya.ru>
2022-10-22 19:38:38 +0300

    edited

--- hw-3/hw-3.2.py
+++ hw-3/hw-3.2.py
@@ -1,2 +1,12 @@
-from hw_3_module_2 import reading
-reading
+from hw_3_module_2 import reading, calcs
+
+expr = calcs()
+res = {
+        expr[1] == '+': int(expr[0]) + int(expr[2]),
+        expr[1] == '-': int(expr[0]) - int(expr[2]),
+        expr[1] == '*': int(expr[0]) * int(expr[2]),
+        expr[1] == '/' and expr[2] != '0': int(expr[0]) / int(expr[2]) if expr[2] != '0' else 'error',
+        expr[1] not in ['+', '-', '*', '/']: 'error'
+    }
+       
+print(res[True])
